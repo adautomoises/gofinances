2ad33098a7c50006bd30ae07a500ee87b26eb23a
adautomoises <adautomaleandro@gmail.com>
2022-08-26 11:26:38 -0300

    Added Fonts, Theme, SplashScreen, Icon

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
-
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 import theme from './src/global/styles/theme'
-
 import { Dashboard  } from './src/screens/Dashboard';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  if(fontsLoaded){
+    SplashScreen.hideAsync();
+  }
+  if(!fontsLoaded){
+    return null
+  }
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
