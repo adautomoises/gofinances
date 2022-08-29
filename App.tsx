be16fd4fc49bdfc37f2fd67ccdbeead759f719c7
adautomoises <adautomaleandro@gmail.com>
2022-08-29 10:25:29 -0300

    Screen Register

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 import theme from './src/global/styles/theme'
-import { Dashboard  } from './src/screens/Dashboard';
+import { Register  } from './src/screens/Register';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,7 +21,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
