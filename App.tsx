cfd6968193ff4bdf0430484ab13220542e26d79e
adautomoises <adautomaleandro@gmail.com>
2022-09-12 11:23:13 -0300

    Tab Navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 import theme from './src/global/styles/theme'
-import { Register  } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes  } from './src/routes/app.routes';
+
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,7 +23,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
