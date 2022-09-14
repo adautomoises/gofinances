dfc67bea247a3f788d2bdc2cec7a400728253bfd
adautomoises <adautomaleandro@gmail.com>
2022-09-14 12:56:10 -0300

    Async Storage

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,98 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { HighLightCard } from "../../components/HighlightCard";
 import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
-import { 
-    Container, 
-    Header, 
-    UserWrapper, 
-    UserInfo, 
-    Photo, 
-    User, 
-    UserGreeting, 
-    UserName, 
-    Icon, 
-    HighLightCards, 
-    Transactions, 
-    Title, 
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighLightCards,
+    Transactions,
+    Title,
     TransactionList,
-    LogoutButton 
+    LogoutButton
 } from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export interface DataListProps extends TransactionCardProps {
     id: string;
 }
 
 export function Dashboard() {
-    const data: DataListProps[] = [
-    {
-        id: '1',
-        type: 'positive',
-        title:"Desenvolvimento de app",
-        amount:"R$ 12.000,00",
-        category: {
-            name: 'Vendas',
-            icon: 'dollar-sign'
-         },
-        date: "28/08/2022"
-    },{
-        id: '2',
-        type: 'negative',
-        title:"Amigão Benfica",
-        amount:"R$ 59,00",
-        category: {
-            name: 'Alimentos',
-            icon: 'coffee'
-         },
-        date: "22/08/2022"
-    },{
-        id: '3',
-        type: 'negative',
-        title:"Aluguel do Apartamento",
-        amount:"R$ 1.200,00",
-        category: {
-            name: 'Casa',
-            icon: 'home'
-         },
-        date: "01/08/2022"
+    const [data, setData] = useState<DataListProps[]>([]);
+
+    async function loadTransactions() {
+        const dataKey = '@gofinances:transactions';
+        const response = await AsyncStorage.getItem(dataKey);
+        const transactions = response ? JSON.parse(response) : [];
+
+        const transactionsFormatted: DataListProps[] = transactions
+        .map((item: DataListProps) => {
+            const amount = Number(item.amount).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            });
+
+            const date = Intl.DateTimeFormat('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: '2-digit'
+            }).format(new Date(item.date));
+
+            return(
+                id: item.id,
+                name: item.name,
+                amount,
+                type: item.type,
+                category: item.category,
+                date,
+            )
+        });
     }
-    ];
+    useEffect(() => {
+        loadTransactions();
+    },[]);
 
-return (
-    <Container>
-    <StatusBar style="light" />
-    <Header>
-        <UserWrapper>
-            <UserInfo>
-                <Photo 
-                    source={{uri: "https://avatars.githubusercontent.com/u/74927238?v=4"}}
-                    />
-                <User>
-                    <UserGreeting>Olá,</UserGreeting>
-                    <UserName>Adauto</UserName>
-                </User>
-            </UserInfo>
-                <LogoutButton onPress={() => {}}>
-                    <Icon name="power"/>
-                </LogoutButton>
-        </UserWrapper>
-    </Header>
+    return (
+        <Container>
+            <StatusBar style="light" />
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: "https://avatars.githubusercontent.com/u/74927238?v=4" }}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Adauto</UserName>
+                        </User>
+                    </UserInfo>
+                    <LogoutButton onPress={() => { }}>
+                        <Icon name="power" />
+                    </LogoutButton>
+                </UserWrapper>
+            </Header>
 
-    <HighLightCards>
-        <HighLightCard type="up" title="Entradas" amount="R$ 12.000,00" lastTransaction="Última entrada dia 28 de agosto"/>
-        <HighLightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 22 de agosto"/>
-        <HighLightCard type="total" title="Total" amount="R$ 10.741,00" lastTransaction="01 à 30 de agosto"/>
-    </HighLightCards>
+            <HighLightCards>
+                <HighLightCard type="up" title="Entradas" amount="R$ 12.000,00" lastTransaction="Última entrada dia 28 de agosto" />
+                <HighLightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 22 de agosto" />
+                <HighLightCard type="total" title="Total" amount="R$ 10.741,00" lastTransaction="01 à 30 de agosto" />
+            </HighLightCards>
 
-    <Transactions>
-        <Title>Listagem</Title>
-        <TransactionList 
-            data={data}
-            keyExtractor = {(item: { id: string; }) => item.id}
-            renderItem={({item}) => <TransactionCard data={item}/>}
-        />
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList
+                    data={data}
+                    keyExtractor={(item: { id: string; }) => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
 
-    </Transactions>
-  </Container>
-)
+            </Transactions>
+        </Container>
+    )
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import { Modal, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
+import { useNavigation, NavigationProp, ParamListBase } from '@react-navigation/native';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
-import {
-  Container,
-  Header,
-  Title,
-  Form,
-  Fields,
-  TransactionTypes,
-} from './styles';
+
+import { Container, Header, Title, Form, Fields, TransactionTypes } from './styles';
+
 import { InputForm } from '../../components/Forms/InputForm'
-import { Input } from '../../components/Forms/Input';
 import { Button } from '../../components/Forms/Button';
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
 import { CategorySelectButton } from '../../components/Forms/CategorySelectButton';
 import { CategorySelect } from '../CategorySelect';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 interface FormData {
   [name: string]: any;
@@ -24,49 +22,78 @@ interface FormData {
 
 const schema = Yup.object().shape({
   name: Yup.string()
-  .required('Nome é obrigatório'),
+    .required('Nome é obrigatório'),
   amount: Yup.number()
-  .typeError('Informe um valor númerico')
-  .positive('O valor não pode ser negativo')
-  .required('O Preço é obrigatório')
+    .typeError('Informe um valor númerico')
+    .positive('O valor não pode ser negativo')
+    .required('O Preço é obrigatório')
 })
 
-export function Register(){
+export function Register() {
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
   const [category, setCategory] = useState({
     key: 'category',
     name: 'Categoria',
   });
-  const { control, handleSubmit, formState: {errors}} = useForm({
+
+  const { navigate }: NavigationProp<ParamListBase> = useNavigation();
+
+  const { control, handleSubmit, reset, formState: { errors } } = useForm({
     resolver: yupResolver(schema)
   });
 
-  function handleTransactionTypeSelect(type: 'up' | 'down'){
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
     setTransactionType(type);
   }
-  function handleOpenSelectCategoryModal(){
+  function handleOpenSelectCategoryModal() {
     setCategoryModalOpen(true);
   }
-  function handleCloseSelectCategoryModal(){
+  function handleCloseSelectCategoryModal() {
     setCategoryModalOpen(false);
   }
-  function handleRegister(form: FormData){
-    if(!transactionType) {
-      return Alert.alert('Selecione o tipo da transação');}
-    if(category.key === 'category') {
-      return Alert.alert('Selecione a categoria');}
-
-
-    const data = {
-      name: form.name,
-      amount: form.amount,
-      transactionType,
-      category: category.key
+  async function handleRegister(form: FormData) {
+    if (!transactionType) {
+      return Alert.alert('Selecione o tipo da transação');
+    }
+    if (category.key === 'category') {
+      return Alert.alert('Selecione a categoria');
     }
 
 
-    console.log(data);
+    const newTransaction = {
+      id: String(uuid.v4()),
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+      date: new Date()
+    }
+
+    try {
+      const dataKey = '@gofinances:transactions';
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria',
+      });
+      navigate('Listagem');
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível salvar");
+    }
   }
 
   return (
@@ -77,14 +104,14 @@ export function Register(){
         </Header>
         <Form>
           <Fields>
-            <InputForm 
+            <InputForm
               name="name"
               control={control}
               placeholder='Nome'
               autoCapitalize="sentences"
               autoCorrect={false}
               error={errors.name && errors.name.message}
-              />
+            />
             <InputForm
               name="amount"
               control={control}
@@ -95,13 +122,13 @@ export function Register(){
 
 
             <TransactionTypes>
-              <TransactionTypeButton 
+              <TransactionTypeButton
                 type="up"
                 title="Income"
                 onPress={() => handleTransactionTypeSelect('up')}
                 isActive={transactionType === 'up'}
               />
-              <TransactionTypeButton 
+              <TransactionTypeButton
                 type="down"
                 title="Outcome"
                 onPress={() => handleTransactionTypeSelect('down')}
@@ -109,23 +136,23 @@ export function Register(){
               />
             </TransactionTypes>
 
-            <CategorySelectButton 
+            <CategorySelectButton
               title={category.name}
-              onPress={handleOpenSelectCategoryModal}  
+              onPress={handleOpenSelectCategoryModal}
             />
           </Fields>
 
-            <Button 
-              title="Enviar"
-              onPress={handleSubmit(handleRegister)}  
-            />
+          <Button
+            title="Enviar"
+            onPress={handleSubmit(handleRegister)}
+          />
         </Form>
 
         <Modal visible={categoryModalOpen}>
-          <CategorySelect 
-              category={category}
-              setCategory={setCategory}
-              closeSelectCategory={handleCloseSelectCategoryModal}
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal}
           />
         </Modal>
       </Container>
