ea2b9a3e1b79ce31e776be50fbdd7ab439517153
adautomoises <adautomaleandro@gmail.com>
2022-10-26 11:46:42 -0300

    Teste em Componentes e Teste com Contexto

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
     '/ios'
   ],
   setupFilesAfterEnv: [
-    "@testing-library/jest-native/extend-expect"
-  ]
+    "@testing-library/jest-native/extend-expect",
+    'jest-styled-components'
+  ],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|react-native-responsive-fontsize|native-base|react-native-svg)"
+  ],
 }
