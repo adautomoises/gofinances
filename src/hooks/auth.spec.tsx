e926193ea0e071b71ce4cc1790059cb0b528cd44
adautomoises <adautomaleandro@gmail.com>
2022-10-29 21:00:41 -0300

    :boom: fix: Module 'ExpoCrypto' Resolvido

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,14 +1,27 @@
-import { renderHook } from '@testing-library/react-native';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from './auth';
 
+const mockStartAsync = jest.fn();
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => mockStartAsync()
+  }
+})
+
+jest.mock('expo-apple-authentication', () => ({}))
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: async () => { }
+}))
 
 describe('Auth Hook', () => {
-  it('should be able to sign in with Google account existent', () => {
+  it('should be able to sign in with Google account existent', async () => {
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     });
 
-    console.log(result);
+    await act( async () => result.current.signInWithGoogle());
 
+    expect(result.current.user).toBeTruthy();
   });
 });
