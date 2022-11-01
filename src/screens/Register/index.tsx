812bb28b0af5d8b25a3b717a1231f9029c298614
adautomoises <adautomaleandro@gmail.com>
2022-11-01 12:37:20 -0300

    :ok_hand: feat: Adicionando testID em elementos

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -142,6 +142,7 @@ export function Register() {
             </TransactionTypes>
 
             <CategorySelectButton
+              testID='button-category'
               title={category.name}
               onPress={handleOpenSelectCategoryModal}
             />
@@ -153,7 +154,7 @@ export function Register() {
           />
         </Form>
 
-        <Modal visible={categoryModalOpen}>
+        <Modal testID="modal-category" visible={categoryModalOpen}>
           <CategorySelect
             category={category}
             setCategory={setCategory}
